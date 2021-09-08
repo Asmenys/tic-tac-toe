5e93ffdcdf1318e7d3a6d69e83c060eb8129cc03
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-09-08 20:58:40 +0300

    First commit, creating the board and the players, displaying the board

---
+++ game.rb
@@ -0,0 +1,48 @@
+require 'pry-byebug'
+#TIC-TAC-TOE
+#a game loop?
+#create players, with a factory class maybe
+#maybe also create the board with a class, how though?
+#how would I check for victory?
+class CREATE_PLAYER
+    attr_reader :player_name, :player_symbol
+    def initialize(player_name)
+        @player_name = player_name
+    end
+    def assign_symbol(player_symbol)
+        @player_symbol = player_symbol
+    end
+end
+#somehow create the objects, then call assign_symbol on them
+#puts "Enter the name for player 1"
+first_player = CREATE_PLAYER.new("P1")
+#puts "Choose a player_symbol to represent you on the board"
+first_player.assign_symbol("Y")
+#puts "Enter the name for player 2"
+second_player = CREATE_PLAYER.new("P2")
+#puts "Choose a player_symbol to represent you on the board"
+second_player.assign_symbol("X")
+class CREATE_BOARD
+    def initialize
+        temp_index=0
+        @board = Array.new(3) {Array.new(3){temp_index+=1}}
+    end
+
+    def display_board
+        puts "| #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]} |"   
+        puts "+-----------+"
+        puts "| #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]} |"
+        puts "+-----------+"
+        puts "| #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]} |"
+    
+    end
+
+end
+game_board = CREATE_BOARD.new
+game_board.display_board
+
+while true
+  return  gets.chomp
+end
+#binding.pry
+#p "something"
